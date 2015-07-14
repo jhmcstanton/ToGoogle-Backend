3371eae1d33b8f7bf15027c4750974421f150f50
Jim Stanton <jim.stanton@cerner.com>
2015-07-14 16:28:53 -0500

    Registered models to admin site

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Note, DataPoint, SearchQuery, Source, Tag
+
+admin.site.register(Note)
+admin.site.register(DataPoint)
+admin.site.register(SearchQuery)
+admin.site.register(Source)
+admin.site.register(Tag)
