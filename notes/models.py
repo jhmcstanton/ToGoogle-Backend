4747c6f9edbb2462abda39d28b4adea2255ae0aa
Jim Stanton <jim.stanton@cerner.com>
2015-07-14 14:08:14 -0500

    Adjusted models, added SearchQuery model

--- notes/models.py
+++ notes/models.py
@@ -5,15 +5,15 @@ class Note(models.Model):
     ''' Basic note model for holding summaries and thoughts for users, as well as linking to DataPoints'''
 
     title               = models.CharField(max_length=30)
-    summary             = models.TextField(max_length=500)
+    summary             = models.TextField(max_length=255)
     creation_date_time  = models.DateTimeField(auto_now_add=True)
     last_edit_date_time = models.DateTimeField(auto_now=True)
     private             = models.BooleanField()
     
     ''' We probaby need owners and people allowed to view this at some point, which will likely require a
      DB migration or rebuild : 
-    # owner =  models.ForeignKey(User)
-    # reviewers = models.ManyToMany(User)
+    # owner =  models.ForeignKey('users.User')
+    # reviewers = models.ManyToMany('users.User')
     '''    
 
     def __str__(self):
@@ -32,6 +32,11 @@ class DataPoint(models.Model):
     is_factual          = models.BooleanField()
     private             = models.BooleanField()
 
+    ''' More user stuff for when we get there
+    # owner = models.ForeignKey('users.User')
+    # reviewers = models.ManyToMany('users.User')
+    '''
+    
     notes               = models.ManyToManyField(Note)
 
     def __str__(self):
@@ -39,3 +44,23 @@ class DataPoint(models.Model):
 
     class Meta:
         ordering = ('last_edit_date_time', 'creation_date_time', )
+
+
+class SearchQuery(models.Model):
+    ''' 
+    Literally just a model to hold previous search queries. PRETTY SWEET.
+    Each Note can have many SearchQuery's, and each SearchQuery is treated
+    as immutable with only one Note.
+    '''
+    query = models.CharField(max_length=255)
+    date_time_queried = models.DateTimeField(auto_now_add=True)
+
+    note              = models.ForeignKey(Note)
+    
+    '''
+    More user stuff. We'll get there soon.
+    # owner = models.ForeignKey('users.User')
+    '''
+
+    class Meta:
+        ordering = ('date_time_queried', )
