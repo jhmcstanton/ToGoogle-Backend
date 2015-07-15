f2feedb1bf8e53c2ecf6c8b3250ea80fd7cfe0ef
Jim Stanton <jim.stanton@cerner.com>
2015-07-14 23:39:55 -0500

    Forgot to update urls

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.test_view, name='test_view'),
-    url(r'^save_note/', views.save_note, name='save_note_view'),
+    #url(r'^$', views.test_view, name='test_view'),
+    url(r'^$', views.handle_note, name='handle_note_view'),
 ]
