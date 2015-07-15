f58afec0981760aed4ffdc4d91f7b80af09455fe
Jim Stanton <jim.stanton@cerner.com>
2015-07-14 20:48:42 -0500

    Setting up super simple note post

--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.test_view, name='test_view'),
+    url(r'^save_note/', views.save_note, name='save_note_view'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.utils import simplejson
+
+from . import models
 
 # Create your views here.
 def test_view(request):
     return JsonResponse({'test':'super_test!'})
+
+def save_note(request):
+    if request.is_ajax() and request.method == 'POST':
+        note_load = simplejson.loads(request.raw_data)
+        try:
+            note = models.Note.create(note_load['title'], note_load['summary'])
+            note.save()
+
+            data_points = []
+            for json_point in note_load['data_points']:
+                point = models.DataPoint.create(json_point, False, False, note)
+                point.save()
+        except KeyError:
+            return HttpResponseError("Malformed JSON Data")
+        return HttpResponse("Received note, saved")
+    return HttpResponseError("Incorrect request")
+
+
