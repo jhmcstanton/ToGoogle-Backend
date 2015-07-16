831e868fce7d6428db8d70cb7e8558cfc78db3ca
Jim Stanton <jim.stanton@cerner.com>
2015-07-16 08:55:21 -0500

    Added additional views to get single notes and list of notes

--- notes/urls.py
+++ notes/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     #url(r'^$', views.test_view, name='test_view'),
-    url(r'^$', views.handle_note, name='handle_note_view'),
+    #url(r'^$', views.handle_note, name='handle_note_view'),
+    url(r'^$', views.list_notes, name='list_notes_view'),
+    url(r'^single/', views.single_note, name='read_single_note_view'),
+    url(r'^update_note/', views.update_single_note, name='update_single_note_view'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse, HttpResponseBadRequest
+from django.contrib.auth import authenticate
 import json
 
 from . import models
@@ -41,3 +42,89 @@ def handle_note(request):
             
     return HttpResponseBadRequest("Incorrect request")
 
+
+def list_notes(request):
+    '''A simple view that only response to ajax GET requests. Assuming the user can authenticate it will return a list of note titles and relevant dates.''' 
+    if request.is_ajax() and request.method == 'GET':
+        payload = json.loads(request.raw_data)
+        user = None
+        try:            
+            user = quick_authenticate(payload)                            
+        except KeyError:
+            return HttpResonseBadRequest("Username or password missing in request.")
+        if user is not None and user.is_active:
+            notes = []
+            for note in models.Note.objects.filter(owner=user):
+                notes.append({note.id:
+                              {'title': note.title,
+                               'creation_date': note.creation_date,
+                               'last_edit_date': note.last_edit_date}
+                })
+                
+                return JsonResponse(notes)
+            else:
+                return HttpResponseBadRequest('Unknown user or incorrect password.')
+            
+    return HttpResponseBadRequest('Not an ajax call.')
+        
+
+def single_note(request):
+    '''A view that lets a user access a single note in it's entirety (READ ONLY)'''
+    if request.is_ajax() and request.method == 'GET':
+        payload = json.loads(request.raw_data)
+        user = None
+        try:
+            user = quick_authenticate(payload)
+        except KeyError:
+            return HttpResponseBadRequest('Malformed ajax request')
+        
+        note = None
+        try:
+            note = Note.objects.filter(owner=user.id).get(id=payload['note_id'])
+        except DoesNotExist:
+            return HttpResponseBadRequest('Unknown note id for this user')
+        
+        data_points = []
+        for point in note.data_points.all():
+        data_points.append({'datum': point.datum,
+                            'is_factual': point.is_factual,
+                            'data_point_id': point.id
+        })                
+        return JsonResponse({'title': note.title, # Does this need the id returned? Front end should be able to cache that
+                             'summary': note.summary,
+                             'data_points': data_points,
+        })                       
+            
+    return HttpResponseBadRequest('Not an ajax call or not a GET request')
+
+def update_single_note(request):
+    '''Allows posting of new or updated notes'''
+    if request.is_ajax() and request.method == 'POST':
+        payload = json.loads(request.raw_data)
+        user = None
+        try:
+            user = quick_authentication(payload)
+            note = None
+            if payload['is_new_note']:
+                note = Note.create(title=payload['title'], summary=payload['summary'], private=True, owner=user)
+                note.save()
+            else:
+                try:
+                    note = Note.objects.filter(owner=user.id).get(id=payload['note_id'])
+                except DoesNotExist:
+                    return HttpResponseBadRequest('Unknown note_id for this user')
+            for data_point in payload['data_points']:
+                try: # Edit the datum on an existing note if it exists
+                    pt_to_edit = note.data_points.get(data_point['data_point_id'])
+                    pt_to_edit.datum = data_point['datum'] # Could throw a KeyError
+                    pt_to_edit.save()
+                except (DoesNotExist, KeyError): # Make a new note if it could not be found
+                    note.data_points.create(datum=data_point['datum'], is_factual=False, private=True, owner=user)                
+            return HttpResponse('Received POST') 
+        except KeyError:
+            return HttpResponseBadRequest('Malformed JSON in note update request')        
+    return HttpResponseBadRequest('Not an ajax call or not a POST request')
+
+
+def quick_authenticate(payload):
+    return authenticate(username=payload['username'], password=payload['password'])
